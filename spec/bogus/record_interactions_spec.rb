5c84a6b86dd5fc4e391a101a8a299dfabfeb7409
Adam Pohorecki <adam.pohorecki@llp.pl>
2012-06-18 22:06:19 +0200

    Tested RecordInteractions, which was impossible without rr

--- spec/bogus/record_interactions_spec.rb
+++ spec/bogus/record_interactions_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
-describe Bogus::RecordInteractions,
-  pending: 'requires rewriting specs to use rr' do
+describe Bogus::RecordInteractions do
 
   class SampleRecordsInteractions
     include Bogus::RecordInteractions
@@ -11,8 +10,18 @@ describe Bogus::RecordInteractions,
   let!(:rr) { Bogus::RRProxy }
 
   it "allows verifying that interactions happened" do
+    sample.__record__(:foo, 1, 2, 3)
+
+    sample.__inner_object__.should have_received.foo(1, 2, 3)
   end
 
   it "allows verifying that interactions didn't happen" do
+    sample.__record__(:bar)
+
+    sample.__inner_object__.should_not have_received.foo
+  end
+
+  it "returns self from record" do
+    sample.__record__(:foo).should == sample
   end
 end
