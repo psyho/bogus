3c22d43578850ab3c5733fe33fc6dd9e68127cad
Piotr Szotkowski <chastell@chastell.net>
2013-05-18 15:47:22 +0200

    side-step warnings on symlinked $GEM_HOME

--- lib/bogus.rb
+++ lib/bogus.rb
@@ -8,7 +8,8 @@ require_relative 'bogus/proxies_method_calls'
 require_relative 'bogus/rspec_extensions'
 require_relative 'bogus/rspec_adapter'
 
-Dir[File.expand_path('../bogus/**/*.rb', __FILE__)].sort.each do |path|
+dir = File.realpath File.expand_path('../bogus', __FILE__)
+Dir["#{dir}/**/*.rb"].sort.each do |path|
   next if path.include? 'bogus/minitest'
   next if path.include? 'bogus/rspec'
   require path
