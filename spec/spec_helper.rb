051378872cf380ce6eb38b2ce3e361c0d1404cb4
Pawel Pierzchala <pawelpierzchala@gmail.com>
2013-05-25 10:06:26 +0200

    Remove specs from coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,13 @@
+require 'simplecov'
 require 'coveralls'
-Coveralls.wear!
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter]
+
+SimpleCov.start do
+  add_filter "/spec/"
+end
 
 require 'bogus'
 require 'dependor/rspec'
