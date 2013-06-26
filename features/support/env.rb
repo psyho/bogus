c20f7a188ff0bdf9adfbd875712b9fd7fc1a95ae
Adam Pohorecki <adam@pohorecki.pl>
2013-06-26 15:55:51 +0200

    Run each feature in a separate directory

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,3 +3,9 @@ require 'aruba/cucumber'
 Before('@known_bug') do
   pending("This scenario fails because of a known bug")
 end
+
+Before do |scenario|
+  dir_name = "scenario-#{rand(1_000_000)}"
+  create_dir(dir_name)
+  cd(dir_name)
+end
