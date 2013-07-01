459c02ce069ee8e246f2e7869964e29d0b061236
Pawel Pierzchala <pawelpierzchala@gmail.com>
2013-07-01 12:58:04 +0200

    Ignore warnings from external libs

--- spec/bogus/clean_ruby_spec.rb
+++ spec/bogus/clean_ruby_spec.rb
@@ -2,6 +2,14 @@ require 'spec_helper'
 
 describe "Ruby syntax" do
   it "is clean" do
-    `ruby -w lib/bogus.rb 2>&1`.should == ''
+    bogus_warnings.should == []
+  end
+
+  def bogus_warnings
+    warnings.select { |w| w =~ %r{lib/bogus} }
+  end
+
+  def warnings
+    `ruby -w lib/bogus.rb 2>&1`.split("\n")
   end
 end
